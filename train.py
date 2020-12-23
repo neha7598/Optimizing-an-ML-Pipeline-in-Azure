3c6d090784af5cc67193be1ffcfd41e7e78a26b4
Neha Dohare <nehadohare@gmail.com>
2020-12-23 23:53:21 +0530

    fix train.py

--- train.py
+++ train.py
@@ -10,20 +10,8 @@ import pandas as pd
 from azureml.core.run import Run
 from azureml.data.dataset_factory import TabularDatasetFactory
 
-# TODO: Create TabularDataset using TabularDatasetFactory
-# Data is located at:
-# "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
 
-path_to_data="https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
-ds = TabularDatasetFactory.from_delimited_files(path=path_to_data)
 
-x, y = clean_data(ds)
-
-# TODO: Split data into train and test sets.
-
-x_train, x_test, y_train, y_test= test_train_split(x, y, test_size=0.20)
-
-run = Run.get_context()
 
 def clean_data(data):
     # Dict for cleaning data
@@ -50,6 +38,8 @@ def clean_data(data):
     x_df["poutcome"] = x_df.poutcome.apply(lambda s: 1 if s == "success" else 0)
 
     y_df = x_df.pop("y").apply(lambda s: 1 if s == "yes" else 0)
+
+    return x_df, y_df
     
 
 def main():
@@ -61,6 +51,21 @@ def main():
 
     args = parser.parse_args()
 
+    # TODO: Create TabularDataset using TabularDatasetFactory
+    # Data is located at:
+    # "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
+
+    path_to_data="https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
+    ds = TabularDatasetFactory.from_delimited_files(path=path_to_data)
+
+    x, y = clean_data(ds)
+
+    # TODO: Split data into train and test sets.
+
+    x_train, x_test, y_train, y_test= test_train_split(x, y, test_size=0.20)
+
+    run = Run.get_context()
+
     run.log("Regularization Strength:", np.float(args.C))
     run.log("Max iterations:", np.int(args.max_iter))
 
@@ -68,6 +73,9 @@ def main():
 
     accuracy = model.score(x_test, y_test)
     run.log("Accuracy", np.float(accuracy))
+
+    os.makedirs('./outputs', exist_ok=True)
+    joblib.dump(value=model,filename='./outputs/model.joblib')
 
 if __name__ == '__main__':
     main()
